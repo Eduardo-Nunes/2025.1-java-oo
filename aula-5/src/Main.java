23266cd73f2c661d7df0ca4da3f0da610ca228b2
Eduardo-Nunes <eduardonunes056@gmail.com>
2025-03-17 20:34:30 -0300

    added menu itens :wip:

--- aula-5/src/Main.java
+++ aula-5/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import pratica1.Aluno;
 import pratica1.Pessoa;
 import pratica2.Barco;
@@ -38,6 +40,76 @@ public class Main {
 
         barco.infosVeiculo();
 
+
+    }
+     private static void menu() {
+        int option = 10;
+        Scanner sc = new Scanner(System.in);
+
+        while (option > 0) {
+            System.out.println("--------- Inicio do Programa ---------");
+            System.out.println("---------------- MENU ----------------");
+            System.out.println("1 - Cadastrar Pessoa ");
+            System.out.println("2 - Cadastrar Aluno ");
+            System.out.println("3 - Cadastrar Veiculo ");
+            System.out.println("4 - Cadastrar Carro ");
+            System.out.println("5 - Cadastrar Moto ");
+            System.out.println("6 - Cadastrar Barco ");
+            System.out.println("0 - Sair ");
+
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    option1();
+                    break;
+                case 2:
+                    option2();
+                    break;
+                case 3:
+                    option3();
+                    break;
+                case 4:
+                    option4();
+                    break;
+                case 5:
+                    option5();
+                    break;
+                case 6:
+                    option6();
+                    break;
+                case 0:
+                    System.out.println("Tchau");
+                    break;
+                default:
+                    System.out.println("opção inválida");
+                    break;
+            }
+        }
+
+    }
+
+    private static void option1() {
+
+    }
+
+    private static void option2() {
+
+    }
+
+    private static void option3() {
+
+    }
+
+    private static void option4() {
+
+    }
+
+    private static void option5() {
+
+    }
+
+    private static void option6() {
 
     }
 }
